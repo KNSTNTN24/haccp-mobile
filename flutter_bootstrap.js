c05bea7f9315d4ec3023fbbcd1dee04126cff24a
KNSTNTN24 <info@limpleymill.co.uk>
2026-03-16 13:48:15 +0000

    Deploy: fix checklist history

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2507561733" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2023275827" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
